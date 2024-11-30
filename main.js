2c26585f95593abd3ee14619ecca0f34e1c216f5
ArtemSt <129779631+milione7777@users.noreply.github.com>
2024-11-30 17:14:09 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
-// Функція для лінивого завантаження зображень
 document.addEventListener("DOMContentLoaded", () => {
   const lazyImages = document.querySelectorAll("img.lazy-load");
 
-  // Створюємо IntersectionObserver
   const observer = new IntersectionObserver((entries, observer) => {
       entries.forEach(entry => {
           if (entry.isIntersecting) {
               const img = entry.target;
-              img.src = img.dataset.src; // Встановлюємо значення src з data-src
-              img.onload = () => img.classList.add("loaded"); // Додаємо клас для анімації після завантаження
-              observer.unobserve(img); // Перестаємо спостерігати за зображенням
+              img.src = img.dataset.src; 
+              img.onload = () => img.classList.add("loaded");
+              observer.unobserve(img); 
           }
       });
   }, {
-      rootMargin: "50px", // Можна налаштувати відступи завчасного завантаження
+      rootMargin: "50px", 
       threshold: 0.1
   });
 
-  // Спостерігаємо за кожним зображенням з атрибутом data-src
   lazyImages.forEach(img => observer.observe(img));
 
-  // Завантаження зображень при натисканні на кнопку
   document.getElementById("loadImages").addEventListener("click", () => {
       lazyImages.forEach(img => {
           if (!img.src) {
